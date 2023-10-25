1550f47b394f16a8630f716bb84ffc674db12bfe
lamafab <42901763+lamafab@users.noreply.github.com>
2023-10-25 15:49:51 +0000

    add PostInvoiceBuilder with invoice method

--- libs/sdk-core/src/offline_requests.rs
+++ libs/sdk-core/src/offline_requests.rs
@@ -1,4 +1,6 @@
-use crate::{LspInformation, NodeState, INVOICE_PAYMENT_FEE_EXPIRY_SECONDS, ensure_sdk, ReceivePaymentRequest, error::{SdkError, SdkResult}, parse_short_channel_id, ChannelState, Peer};
+use lightning_invoice::RawInvoice;
+
+use crate::{LspInformation, NodeState, INVOICE_PAYMENT_FEE_EXPIRY_SECONDS, ensure_sdk, ReceivePaymentRequest, error::{SdkError, SdkResult}, parse_short_channel_id, ChannelState, Peer, parse_invoice, RouteHint, RouteHintHop, invoice::add_lsp_routing_hints};
 
 pub struct NodeApiRequestBuilder;
 
@@ -11,7 +13,7 @@ impl NodeApiRequestBuilder {
 pub struct PaymentReceiverBuilder;
 
 impl PaymentReceiverBuilder {
-	pub fn lsp_information(req: ReceivePaymentRequest,lsp_info: LspInformation, node_state: NodeState, peers: Option<Vec<Peer>>) -> SdkResult<()>{
+	pub fn lsp_information(req: ReceivePaymentRequest, lsp_info: LspInformation, node_state: NodeState, peers: Option<Vec<Peer>>) -> SdkResult<()>{
         let expiry = req.expiry.unwrap_or(INVOICE_PAYMENT_FEE_EXPIRY_SECONDS);
 
         ensure_sdk!(
@@ -80,6 +82,93 @@ impl PaymentReceiverBuilder {
             }
         }
 
+		/*
+        info!("Creating invoice on NodeAPI");
+        let invoice = &self
+            .node_api
+            .create_invoice(
+                destination_invoice_amount_msat,
+                req.description,
+                req.preimage,
+                req.use_description_hash,
+                Some(expiry),
+                Some(req.cltv.unwrap_or(144)),
+            )
+            .await?;
+        info!("Invoice created {}", invoice);
+
+        let mut parsed_invoice = parse_invoice(invoice)?;
+		*/
+
 		todo!()
 	}
+}
+
+pub struct PostInvoiceBuilder {
+	req: ReceivePaymentRequest,
+	lsp_info: LspInformation,
+	open_channel_needed: bool,
+	short_channel_id: u64,
+	destination_invoice_amount_msat: u64,
+	invoice: String,
+}
+
+impl PostInvoiceBuilder {
+	pub fn invoice(self, invoice: &str) -> SdkResult<UpdatedInvoiceContext> {
+        let mut parsed_invoice = parse_invoice(invoice)?;
+
+		// TODO: Extra checks between `parsed_invoice` and `self.*`?
+
+        // check if the lsp hint already exists
+        info!("Existing routing hints {:?}", parsed_invoice.routing_hints);
+        info!("lsp info pubkey = {:?}", self.lsp_info.pubkey.clone());
+        let has_lsp_hint = parsed_invoice.routing_hints.iter().any(|h| {
+            h.hops
+                .iter()
+                .any(|h| h.src_node_id == self.lsp_info.pubkey.clone())
+        });
+
+        // We only add routing hint if we need to open a channel
+        // or if the invoice doesn't have any routing hints that points to the lsp
+        let mut lsp_hint: Option<RouteHint> = None;
+        if !has_lsp_hint || self.open_channel_needed {
+            let lsp_hop = RouteHintHop {
+                src_node_id: self.lsp_info.pubkey,
+                short_channel_id: self.short_channel_id,
+                fees_base_msat: self.lsp_info.base_fee_msat as u32,
+                fees_proportional_millionths: (self.lsp_info.fee_rate * 1000000.0) as u32,
+                cltv_expiry_delta: self.lsp_info.time_lock_delta as u64,
+                htlc_minimum_msat: Some(self.lsp_info.min_htlc_msat as u64),
+                htlc_maximum_msat: None,
+            };
+
+            info!("Adding LSP hop as routing hint: {:?}", lsp_hop);
+            lsp_hint = Some(RouteHint {
+                hops: vec![lsp_hop],
+            });
+        }
+
+        // We only create a new invoice if we need to add the lsp hint or change the amount
+		let mut new_invoice = None;
+        if lsp_hint.is_some() || self.req.amount_msat != self.destination_invoice_amount_msat {
+            // create the large amount invoice
+            let raw_invoice_with_hint =
+                add_lsp_routing_hints(self.invoice.clone(), lsp_hint, self.req.amount_msat)?;
+
+            info!("Routing hint added");
+			new_invoice = Some(raw_invoice_with_hint);
+			// TODO:
+            //info!("Signed invoice with hint = {}", signed_invoice_with_hint);
+        }
+
+		Ok(UpdatedInvoiceContext {
+			new_invoice,
+		})
+	}
+}
+
+pub struct UpdatedInvoiceContext {
+	// We only create a new invoice if we need to add the lsp hint or change the amount
+	// TODO: Expand.
+	new_invoice: Option<RawInvoice>,
 }

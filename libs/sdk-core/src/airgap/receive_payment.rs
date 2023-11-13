b12d72288556776f5ec292edd99ab977f2216449
lamafab <42901763+lamafab@users.noreply.github.com>
2023-11-13 17:26:00 +0000

    implement check_lsp_hints, WIP

--- libs/sdk-core/src/airgap/receive_payment.rs
+++ libs/sdk-core/src/airgap/receive_payment.rs
@@ -1,19 +1,30 @@
 use crate::{
-    ensure_sdk, error::ReceivePaymentError, parse_short_channel_id, LspInformation, NodeState,
-    ReceivePaymentRequest, ReceivePaymentResponse, Peer, ChannelState,
+    ensure_sdk, error::ReceivePaymentError, grpc::PaymentInformation,
+    invoice::add_lsp_routing_hints, parse_invoice, parse_short_channel_id, ChannelState,
+    LspInformation, NodeState, OpeningFeeParams, Peer, ReceivePaymentRequest,
+    ReceivePaymentResponse, RouteHint, RouteHintHop,
 };
 use anyhow::anyhow;
 use gl_client::pb::cln::{self, listinvoices_invoices::ListinvoicesInvoicesStatus, ListpeersPeers};
+use lightning_invoice::RawInvoice;
+use serde_json::json;
 
 pub const INVOICE_PAYMENT_FEE_EXPIRY_SECONDS: u32 = 60 * 60; // 60 minutes
 
-pub fn receive_payment(
+pub struct PreparedInvoiceContext {
+    short_channel_id: u64,
+    destination_invoice_amount_msat: u64,
+    channel_opening_fee_params: Option<OpeningFeeParams>,
+    open_channel_needed: bool,
+}
+
+pub fn prepare_invoice(
     lsp_info: LspInformation,
     node_state: NodeState,
     node_peers: cln::ListpeersResponse,
     req: ReceivePaymentRequest,
 ) -> Result<ReceivePaymentResponse, ReceivePaymentError> {
-	let expiry = req.expiry.unwrap_or(INVOICE_PAYMENT_FEE_EXPIRY_SECONDS);
+    let expiry = req.expiry.unwrap_or(INVOICE_PAYMENT_FEE_EXPIRY_SECONDS);
 
     ensure_sdk!(
         req.amount_msat > 0,
@@ -59,8 +70,8 @@ pub fn receive_payment(
     } else {
         // not opening a channel so we need to get the real channel id into the routing hints
         info!("Finding channel ID for routing hint");
-		// TODO: Remove cloned
-		let peer_models: Vec<Peer> = node_peers.peers.into_iter().map(|p| p.into()).collect();
+        // TODO: Remove cloned
+        let peer_models: Vec<Peer> = node_peers.peers.into_iter().map(|p| p.into()).collect();
         for peer in peer_models {
             if hex::encode(peer.id) == lsp_info.pubkey && !peer.channels.is_empty() {
                 let active_channel = peer
@@ -79,6 +90,117 @@ pub fn receive_payment(
             }
         }
     }
+
+    let ctx = PreparedInvoiceContext {
+        short_channel_id,
+        destination_invoice_amount_msat,
+        channel_opening_fee_params,
+        open_channel_needed,
+    };
+
+    todo!()
+}
+
+pub struct CheckedLspHintsContext {
+    // TODO: Remove, signing should happen in `check_lsp_hints`
+    checked_invoice: Option<RawInvoice>,
+    payment_info: Option<PaymentInfo>,
+}
+
+pub struct PaymentInfo {
+    payment_hash: Vec<u8>,
+    payment_secret: Vec<u8>,
+    destination: Vec<u8>,
+    incoming_amount_msat: i64,
+    outgoing_amount_msat: i64,
+    opening_fee_params: Option<OpeningFeeParams>,
+}
+
+fn check_lsp_hints(
+    ctx: PreparedInvoiceContext,
+    invoice: String,
+    lsp_info: LspInformation,
+    req: ReceivePaymentRequest,
+) -> Result<ReceivePaymentResponse, ReceivePaymentError> {
+    let mut parsed_invoice = parse_invoice(&invoice)?;
+
+    // check if the lsp hint already exists
+    info!("Existing routing hints {:?}", parsed_invoice.routing_hints);
+    info!("lsp info pubkey = {:?}", lsp_info.pubkey.clone());
+    let has_lsp_hint = parsed_invoice.routing_hints.iter().any(|h| {
+        h.hops
+            .iter()
+            .any(|h| h.src_node_id == lsp_info.pubkey.clone())
+    });
+
+    // We only add routing hint if we need to open a channel
+    // or if the invoice doesn't have any routing hints that points to the lsp
+    let mut lsp_hint: Option<RouteHint> = None;
+    if !has_lsp_hint || ctx.open_channel_needed {
+        let lsp_hop = RouteHintHop {
+            src_node_id: lsp_info.pubkey,
+            short_channel_id: ctx.short_channel_id,
+            fees_base_msat: lsp_info.base_fee_msat as u32,
+            fees_proportional_millionths: (lsp_info.fee_rate * 1000000.0) as u32,
+            cltv_expiry_delta: lsp_info.time_lock_delta as u64,
+            htlc_minimum_msat: Some(lsp_info.min_htlc_msat as u64),
+            htlc_maximum_msat: None,
+        };
+
+        info!("Adding LSP hop as routing hint: {:?}", lsp_hop);
+        lsp_hint = Some(RouteHint {
+            hops: vec![lsp_hop],
+        });
+    }
+
+    // We only create a new invoice if we need to add the lsp hint or change the amount
+    let mut raw_invoice_with_hint = None;
+    if lsp_hint.is_some() || req.amount_msat != ctx.destination_invoice_amount_msat {
+        // create the large amount invoice
+        raw_invoice_with_hint = Some(add_lsp_routing_hints(
+            invoice.clone(),
+            lsp_hint,
+            req.amount_msat,
+        )?);
+
+        // TODO: Signing should happen here.
+
+        /* TODO
+        info!("Routing hint added");
+        let signed_invoice_with_hint = self.node_api.sign_invoice(raw_invoice_with_hint)?;
+        info!("Signed invoice with hint = {}", signed_invoice_with_hint);
+
+        parsed_invoice = parse_invoice(&signed_invoice_with_hint)?;
+        */
+    }
+
+    // register the payment at the lsp if needed
+    let mut payment_info = None;
+    if ctx.open_channel_needed {
+        info!("Registering payment with LSP");
+
+        if ctx.channel_opening_fee_params.is_none() {
+            return Err(ReceivePaymentError::Generic {
+                err: "We need to open a channel, but no channel opening fee params found".into(),
+            });
+        }
+
+        payment_info = Some(PaymentInfo {
+            payment_hash: hex::decode(parsed_invoice.payment_hash.clone())
+                .map_err(|e| anyhow!("Failed to decode hex payment hash: {e}"))?,
+            payment_secret: parsed_invoice.payment_secret.clone(),
+            destination: hex::decode(parsed_invoice.payee_pubkey.clone())
+                .map_err(|e| anyhow!("Failed to decode hex payee pubkey: {e}"))?,
+            incoming_amount_msat: req.amount_msat as i64,
+            outgoing_amount_msat: ctx.destination_invoice_amount_msat as i64,
+            opening_fee_params: ctx.channel_opening_fee_params.clone().map(Into::into),
+        });
+    }
+
+    let ctx = CheckedLspHintsContext {
+        checked_invoice: raw_invoice_with_hint,
+        payment_info,
+    };
 
     todo!()
 }
